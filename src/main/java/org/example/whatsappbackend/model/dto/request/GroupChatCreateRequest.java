41ef36dd8eca9bee33e14b88a8b30351778a90f0
Md. Giash Uddin <giash.inument@bkash.com>
2024-07-13 11:51:19 +0600

    Updated code

--- src/main/java/org/example/whatsappbackend/model/dto/request/GroupChatCreateRequest.java
+++ src/main/java/org/example/whatsappbackend/model/dto/request/GroupChatCreateRequest.java
@@ -1,12 +1,12 @@
 package org.example.whatsappbackend.model.dto.request;
 
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.NotEmpty;
 
 import java.util.List;
 
 public record GroupChatCreateRequest(
-        @NotNull
+        @NotEmpty
         List<Integer> userIds,
         @NotBlank
         String chatName,
